b8ae2646a3cd7522689081baf54464d0436ad850
Lorenayannnnn <lorenayan@qq.com>
2021-10-24 12:51:31 -0700

    init frontend

--- backend/src/main/java/com/backend/backenddemo/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.backend.backenddemo.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-
-
-}
--- backend/src/main/java/com/backend/backenddemo/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.backend.backenddemo.service;
-
-public interface UserService {
-}
