a7f42094377d555d62a46e6124ebf4b4dbebe80d
Lorenayannnnn <lorenayan@qq.com>
2021-10-25 13:53:04 -0700

    init main chat

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+// Element-UI: https://element.eleme.cn/#/zh-CN/component/select
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import Vuesax from 'vuesax';
+import 'vuesax/dist/vuesax.css'
+
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI )
+Vue.use(ElementUI)
+Vue.use(Vuesax)
 
 /* eslint-disable no-new */
 new Vue({
